2d04d41f1dcb0106d5b2d93c929cc999511a9433
Gustavo Rivero <gustavoerivero12@gmail.com>
2022-05-19 22:33:30 -0400

    Fix the PostPage view so that the CommentInput is not hidden

--- src/pages/PostPage.js
+++ src/pages/PostPage.js
@@ -71,182 +71,187 @@ const CreatePostPage = () => {
   )
 
   return (
-    <VStack h={layout.height - 50}>
-      <VStack>
-        <Stack bgColor='white' alignItems='center' py={3} pt={4}>
-          <Text bold fontSize={20} color={COLORS.primary}>
-            Comparte tu experiencia
-          </Text>
-        </Stack>
-      </VStack>
-      <ScrollView>
-        <PostModify
-          user={userData}
-          post={post}
-        />
-      </ScrollView>
+    <KeyboardAwareScrollView>
+      <VStack h={layout.height * .94}>
+        <VStack>
+          <Stack bgColor='white' alignItems='center' py={3} pt={4}>
+            <Text bold fontSize={20} color={COLORS.primary}>
+              Comparte tu experiencia
+            </Text>
+          </Stack>
+        </VStack>
+        <ScrollView>
+          <PostModify
+            user={userData}
+            post={post}
+          />
+        </ScrollView>
 
 
-      <VStack justifyContent='flex-end'>
-        <HStack
-          justifyContent='space-between'
-          px={1}
-          pt={3}
-          minW={layout.width}
-        >
-          <Stack justifyContent='flex-end' >
-            {post?.image !== '' && (
-              <Button
+        <VStack justifyContent='flex-end'>
+          <HStack
+            justifyContent='space-between'
+            h={57}
+            px={1}
+            pt={1}
+            minW={layout.width}
+            borderTopWidth={1}
+            borderColor='gray.200'
+            borderRadius={10}
+          >
+            <Stack justifyContent='flex-end' >
+              {post?.image !== '' && (
+                <Button
+                  icon={
+                    <Image
+                      source={{
+                        uri: post.image
+                      }}
+                      style={{
+                        width: 40,
+                        height: 40,
+                        borderRadius: 5,
+                      }}
+                    />
+                  }
+                  type='clear'
+                  containerStyle={{
+                    height: 45,
+                    width: 45,
+                    margin: 5,
+                    marginLeft: 10,
+                    alignContent: 'center',
+                    justifyContent: 'center',
+                  }}
+                  onPress={() => {
+                    console.log(`${post.author}'s post`)
+                    _handleChange('image', '')
+                  }}
+                />
+              )}
+            </Stack>
+            <HStack space={2}>
+
+              <AddTag
+                visible={addPersonDialog}
+                setVisible={setAddPersonDialog}
+                setChoice={setAddPersonChoice}
+                content='¿A quién quieres etiquetar?'
+
+              />
+
+              <FAB
                 icon={
-                  <Image
-                    source={{
-                      uri: post.image
-                    }}
-                    style={{
-                      width: 40,
-                      height: 40,
-                      borderRadius: 5,
-                    }}
+                  <Ionicons
+                    name='ios-person-add'
+                    color='#fff'
+                    size={20}
                   />
                 }
-                type='clear'
                 containerStyle={{
-                  height: 45,
-                  width: 45,
-                  margin: 5,
-                  marginLeft: 10,
-                  alignContent: 'center',
+                  position: 'relative',
+                  marginBottom: 5,
+                  right: '15%',
+                  alignItems: 'center',
                   justifyContent: 'center',
+                  width: 40,
+                  height: 40,
                 }}
+                color='rgba(200, 123, 255, 1)'
                 onPress={() => {
-                  console.log(`${post.author}'s post`)
-                  _handleChange('image', '')
+                  console.log('Add person tag')
+                  setAddPersonDialog(true)
                 }}
               />
-            )}
-          </Stack>
-          <HStack space={2}>
 
-            <AddTag
-              visible={addPersonDialog}
-              setVisible={setAddPersonDialog}
-              setChoice={setAddPersonChoice}
-              content='¿A quién quieres etiquetar?'
+              <FAB
+                icon={
+                  <AntDesign
+                    name='tags'
+                    color='#fff'
+                    size={20}
+                  />
+                }
+                containerStyle={{
+                  position: 'relative',
+                  marginBottom: 5,
+                  right: '15%',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  width: 40,
+                  height: 40,
+                }}
+                color='rgba(90, 123, 255, 1)'
+                onPress={() => {
+                  console.log('Add post tag')
+                  setAddTagDialog(true)
+                }}
+              />
 
-            />
-
-            <FAB
-              icon={
-                <Ionicons
-                  name='ios-person-add'
-                  color='#fff'
-                  size={20}
-                />
-              }
-              containerStyle={{
-                position: 'relative',
-                marginBottom: 5,
-                right: '15%',
-                alignItems: 'center',
-                justifyContent: 'center',
-                width: 40,
-                height: 40,
-              }}
-              color='rgba(200, 123, 255, 1)'
-              onPress={() => {
-                console.log('Add person tag')
-                setAddPersonDialog(true)
-              }}
-            />
-
-            <FAB
-              icon={
-                <AntDesign
-                  name='tags'
-                  color='#fff'
-                  size={20}
-                />
-              }
-              containerStyle={{
-                position: 'relative',
-                marginBottom: 5,
-                right: '15%',
-                alignItems: 'center',
-                justifyContent: 'center',
-                width: 40,
-                height: 40,
-              }}
-              color='rgba(90, 123, 255, 1)'
-              onPress={() => {
-                console.log('Add post tag')
-                setAddTagDialog(true)
-              }}
-            />
-
-            <FAB
-              icon={
-                <Ionicons
-                  name='ios-image-outline'
-                  color='#fff'
-                  size={20}
-                />
-              }
-              containerStyle={{
-                position: 'relative',
-                marginBottom: 5,
-                right: '15%',
-                alignItems: 'center',
-                justifyContent: 'center',
-                width: 40,
-                height: 40,
-              }}
-              color='rgba(90, 85, 220, 1)'
-              onPress={() => {
-                console.log('Upload image')
-                let image = pickImage()
-                image.then(res => {
-                  _handleChange('image', res.uri)
-                  console.log(res)
-                }).catch(err => {
-                  console.log(err)
-                })
-              }}
-            />
+              <FAB
+                icon={
+                  <Ionicons
+                    name='ios-image-outline'
+                    color='#fff'
+                    size={20}
+                  />
+                }
+                containerStyle={{
+                  position: 'relative',
+                  marginBottom: 5,
+                  right: '15%',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  width: 40,
+                  height: 40,
+                }}
+                color='rgba(90, 85, 220, 1)'
+                onPress={() => {
+                  console.log('Upload image')
+                  let image = pickImage()
+                  image.then(res => {
+                    _handleChange('image', res.uri)
+                    console.log(res)
+                  }).catch(err => {
+                    console.log(err)
+                  })
+                }}
+              />
+            </HStack>
           </HStack>
-        </HStack>
 
-        <CommentInput
-          value={post.description}
-          onChangeText={(text) => _handleChange('description', text)}
-          placeholder='Dinos, ¿qué opinas?...'
-          rightElement={
-            <FAB
-              icon={
-                <FontAwesome
-                  name='send'
-                  color='#fff'
-                  size={20}
-                />
-              }
-              color='#b973ff'
-              containerStyle={{
-                position: 'relative',
-                marginBottom: 5,
-                right: '5%',
-                alignItems: 'center',
-                justifyContent: 'center',
-                width: 50,
-                height: 50,
-              }}
-              onPress={() => {
-                console.log('Send')
-              }}
-            />
-          }
-        />
+          <CommentInput
+            value={post.description}
+            onChangeText={(text) => _handleChange('description', text)}
+            placeholder='Dinos, ¿qué opinas?...'
+            rightElement={
+              <FAB
+                icon={
+                  <FontAwesome
+                    name='send'
+                    color='#fff'
+                    size={20}
+                  />
+                }
+                color='#b973ff'
+                containerStyle={{
+                  position: 'relative',
+                  marginBottom: 5,
+                  right: '5%',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  width: 50,
+                  height: 50,
+                }}
+                onPress={() => {
+                  console.log('Send')
+                }}
+              />
+            }
+          />
+        </VStack>
       </VStack>
-    </VStack>
-
+    </KeyboardAwareScrollView>
   )
 }
 
