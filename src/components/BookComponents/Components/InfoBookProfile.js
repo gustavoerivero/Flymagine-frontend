ac53c9c0db6ef8e6f30bf03826cdbff6bddd174d
Gustavo Rivero <gustavoerivero12@gmail.com>
2022-05-27 17:52:18 -0400

    Update search page: book search

--- src/components/BookComponents/Components/InfoBookProfile.js
+++ src/components/BookComponents/Components/InfoBookProfile.js
@@ -1,9 +1,9 @@
-import React, { useCallback, useState, useEffect } from "react";
-import { View, StyleSheet, ScrollView, TouchableOpacity } from "react-native";
+import React, { useCallback, useState, useEffect } from 'react'
+import { View, StyleSheet, ScrollView, TouchableOpacity } from 'react-native'
 
-import { Tab, TabView, AirbnbRating } from "react-native-elements";
+import { Tab, TabView, AirbnbRating } from 'react-native-elements'
 
-import { useNavigation } from "@react-navigation/native";
+import { useNavigation } from '@react-navigation/native'
 
 import {
   Ionicons,
@@ -13,7 +13,7 @@ import {
   AntDesign,
   MaterialCommunityIcons,
   FontAwesome,
-} from "@expo/vector-icons";
+} from '@expo/vector-icons'
 
 import {
   Avatar,
@@ -30,106 +30,114 @@ import {
   Button,
   IconButton,
   Image,
-} from "native-base";
+} from 'native-base'
 
-import useAuthContext from "../../../hooks/useAuthContext";
+import useAuthContext from '../../../hooks/useAuthContext'
 
-import { useFocusEffect } from "@react-navigation/native";
+import { useFocusEffect } from '@react-navigation/native'
 
 //Image
 
 //Components
-import Review from "../../Post/Review";
-import StatusBookButton from "./StatusBookButton";
+import Review from '../../Post/Review'
+import StatusBookButton from './StatusBookButton'
 
 //Data
-import dataReviews from "../../../utilities/data/reviews.json";
-import { getAllLiteraryGenre } from "../../../services/literaryGenre/literaryGenre";
-import { getUserById } from "../../../services/user/userAPI";
+import dataReviews from '../../../utilities/data/reviews.json'
+import { getAllLiteraryGenre } from '../../../services/literaryGenre/literaryGenre'
+import { getUserById } from '../../../services/user/userAPI'
 
 //Colors
-import COLORS from "../../styled-components/Colors";
+import COLORS from '../../styled-components/Colors'
 
 //Styles
-import stylesBook from "../../styled-components/stylesBook";
+import stylesBook from '../../styled-components/stylesBook'
 
 const InfoBookProfile = ({ navigation, bookInfo, author, bookGenres}) => {
   //Colors
-  const backgroundColor = COLORS.primary;
-  const text = COLORS.base;
-  const elementsButton = COLORS.button.text;
-  const icon = COLORS.button.icon;
-  const buttonColor = COLORS.button.terciary;
-  const categoryBgColor = COLORS.button.secundary;
+  const backgroundColor = COLORS.primary
+  const text = COLORS.base
+  const elementsButton = COLORS.button.text
+  const icon = COLORS.button.icon
+  const buttonColor = COLORS.button.terciary
+  const categoryBgColor = COLORS.button.secundary
 
   //Status Book
-  const [buttonToRead, setButtonToRead] = useState(true);
-  const [buttonReading, setButtonReading] = useState(false);
-  const [buttonRead, setButtonRead] = useState(false);
+  const [buttonToRead, setButtonToRead] = useState(true)
+  const [buttonReading, setButtonReading] = useState(false)
+  const [buttonRead, setButtonRead] = useState(false)
 
-  const [index, setIndex] = React.useState(0);
+  const [index, setIndex] = React.useState(0)
 
-  const [show, setShow] = useState(true);
+  const [show, setShow] = useState(true)
 
-  const [signIn, setSignIn] = useState("Adam Meddler");
+  const [signIn, setSignIn] = useState('Adam Meddler')
 
-  const [literaryGenres, setLiteraryGenres] = useState([]);
+  const [literaryGenres, setLiteraryGenres] = useState([])
 
   const {
     state: { user },
-  } = useAuthContext();
+  } = useAuthContext()
 
   useFocusEffect(
     useCallback(() => {
       getAllLiteraryGenre()
         .then((response) => {
-          setLiteraryGenres(response);
+          setLiteraryGenres(response)
         })
         .catch((error) => {
-          console.log(error);
-          showErrorToast("Error al agregar los géneros literarios");
-        });
-      console.log(bookInfo?.photo);
+          console.log(error)
+          showErrorToast('Error al agregar los géneros literarios')
+        })
+      console.log(bookInfo?.photo)
     }, [])
-  );
+  )
 
   return (
-    <Box h="100%" bg={backgroundColor} opacity="0.95">
-      <HStack h="30%">
-        <VStack w="40%" justifyContent="center" alignItems="center">
+    <Box h='100%' bg={backgroundColor} opacity='0.95'>
+      <HStack h='30%'>
+        <VStack w='40%' justifyContent='center' alignItems='center'>
           <Image
             source={{ uri: bookInfo?.photo }}
             style={stylesBook.image}
-            alt="post"
+            alt='post'
           />
         </VStack>
 
-        <VStack w="60%" px={1} justifyContent="center">
-          <HStack h="40%">
-            <Text fontSize="xl" bold color={text}>
+        <VStack w='60%' px={1} justifyContent='center'>
+          <HStack h='40%'>
+            <Text fontSize='xl' bold color={text}>
               {bookInfo && bookInfo?.name}
             </Text>
           </HStack>
 
-          <HStack h="10%">
-            <AirbnbRating count={5} showRating={false} size={16} defaultRating={4} isDisabled={true} selectedColor={'#FF00F0'} unSelectedColor={COLORS.button.secundaryDisabled}/>
+          <HStack h='10%'>
+            <AirbnbRating 
+              count={5} 
+              showRating={false} 
+              size={16} 
+              defaultRating={4} 
+              isDisabled={true} 
+              selectedColor={'#FF00F0'} 
+              unSelectedColor={COLORS.button.secundaryDisabled}
+            />
           </HStack>
 
-          <HStack alignItems="center" pt={1} h="10%">
-            <Ionicons name="person" color={icon} />
-            <Text fontSize="sm" color={text} pl={1}>
-              {author && author?.lastName[0] + ". " + author?.firstName}
+          <HStack alignItems='center' pt={1} h='10%'>
+            <Ionicons name='person' color={icon} />
+            <Text fontSize='sm' color={text} pl={1}>
+              {author && author?.lastName[0] + '. ' + author?.firstName}
             </Text>
           </HStack>
 
-          <HStack alignItems="center" pt={1} h="10%">
-            <Entypo name="book" color={icon} />
-            <Text fontSize="sm" color={text} pl={1}>
+          <HStack alignItems='center' pt={1} h='10%'>
+            <Entypo name='book' color={icon} />
+            <Text fontSize='sm' color={text} pl={1}>
               Géneros: {bookInfo && bookInfo?.geners}
             </Text>
           </HStack>
 
-          <HStack my={2} h="15%">
+          <HStack my={2} h='15%'>
             <Box>
               <FlatList
                 horizontal
@@ -139,9 +147,9 @@ const InfoBookProfile = ({ navigation, bookInfo, author, bookGenres}) => {
                 ItemSeparatorComponent={() => <Box w={1} />}
                 renderItem={({ item }) => (
                   <Badge
-                    colorScheme="success"
-                    alignItems="center"
-                    justifyContent="center"
+                    colorScheme='success'
+                    alignItems='center'
+                    justifyContent='center'
                     style={{
                       borderRadius: 30,
                       backgroundColor: categoryBgColor,
@@ -158,62 +166,62 @@ const InfoBookProfile = ({ navigation, bookInfo, author, bookGenres}) => {
         </VStack>
       </HStack>
 
-      <HStack h="35%" justifyContent="center">
-        <VStack w="95%">
-          <HStack alignItems="center" my={1}>
-            <MaterialIcons name="description" size={12} color={icon} />
-            <Text fontSize="md" bold color={text} pl={1}>
+      <HStack h='35%' justifyContent='center'>
+        <VStack w='95%'>
+          <HStack alignItems='center' my={1}>
+            <MaterialIcons name='description' size={12} color={icon} />
+            <Text fontSize='md' bold color={text} pl={1}>
               Sinopsis:
             </Text>
           </HStack>
 
-          <Text fontSize="md" color={text} pl={1} textAlign="justify">
+          <Text fontSize='md' color={text} pl={1} textAlign='justify'>
             {bookInfo && bookInfo?.sypnosis}
           </Text>
         </VStack>
       </HStack>
 
-      <HStack h="20%" justifyContent="center">
+      <HStack h='20%' justifyContent='center'>
         <VStack>
-          <HStack justifyContent="space-around" h="40%" alignItems="center">
+          <HStack justifyContent='space-around' h='40%' alignItems='center'>
             <Button
-              h="95%"
-              w="80%"
+              h='95%'
+              w='80%'
               bg={buttonColor}
-              colorScheme="blueGray"
+              colorScheme='blueGray'
               endIcon={
                 <Icon
                   as={Ionicons}
-                  name="cloud-download-outline"
-                  size="md"
+                  name='cloud-download-outline'
+                  size='md'
                   color={elementsButton}
                 />
               }
-              borderRadius="full"
+              borderRadius='full'
               onPress={() => [
                 setButtonToRead(false),
                 setButtonReading(true),
                 setButtonRead(false),
               ]}
             >
-              <Text fontSize="md" bold color={elementsButton}>
+              <Text fontSize='md' bold color={elementsButton}>
                 Descargar libro
               </Text>
             </Button>
             <IconButton
-              h="95%"
-              w="15%"
-              variant="unstyled"
-              alignItems="center"
-              justifyContent="center"
+              h='95%'
+              w='15%'
+              variant='unstyled'
+              alignItems='center'
+              justifyContent='center'
               icon={
                 <MaterialIcons
-                  name="favorite"
+                  name='favorite'
                   size={20}
-                  color={show ? icon : "red"}
+                  color={show ? icon : 'red'}
                 />
               }
-              borderRadius="100"
+              borderRadius='100'
               activeOpacity={0.85}
               onPress={() => setShow(!show)}
             />
@@ -221,18 +229,18 @@ const InfoBookProfile = ({ navigation, bookInfo, author, bookGenres}) => {
 
           {author?._id == user?.id ? (
             <HStack
-              justifyContent="space-between"
-              h="30%"
-              alignItems="center"
-              mt="3"
+              justifyContent='space-between'
+              h='30%'
+              alignItems='center'
+              mt='3'
             >
               <Button
-                h="95%"
-                variant="unstyled"
-                borderRadius="full"
-                startIcon={<FontAwesome name="edit" size={15} color={icon} />}
+                h='95%'
+                variant='unstyled'
+                borderRadius='full'
+                startIcon={<FontAwesome name='edit' size={15} color={icon} />}
               >
-                <Text fontSize="md" bold color={text}>
+                <Text fontSize='md' bold color={text}>
                   Editar libro
                 </Text>
               </Button>
@@ -247,10 +255,10 @@ const InfoBookProfile = ({ navigation, bookInfo, author, bookGenres}) => {
             </HStack>
           ) : (
             <HStack
-              justifyContent="flex-end"
-              h="30%"
-              alignItems="center"
-              mt="3"
+              justifyContent='flex-end'
+              h='30%'
+              alignItems='center'
+              mt='3'
             >
               <StatusBookButton
                 buttonRead={buttonRead}
@@ -265,7 +273,7 @@ const InfoBookProfile = ({ navigation, bookInfo, author, bookGenres}) => {
         </VStack>
       </HStack>
     </Box>
-  );
-};
+  )
+}
 
-export default InfoBookProfile;
+export default InfoBookProfile
--- src/services/book/bookAPI.js
+++ src/services/book/bookAPI.js
@@ -3,22 +3,28 @@ import { http } from "../http"
 const BASE_URL = 'book'
 
 const getBooks = async () => {
-    const { data } = await http.get(`${BASE_URL}`)
-    return data?.Data
-  }
-  
-  const getBookById = async (id) => {
-    const { data } = await http.get(`${BASE_URL}/${id}`)
-    return data?.Data
-  }
+  const { data } = await http.get(`${BASE_URL}`)
+  return data?.Data
+}
 
-  const getGenresByIdBook = async (id) => {
-    const { data } = await http.get(`${BASE_URL}/${id}/genres`)
-    return data?.Data
-  }
+const getBookById = async (id) => {
+  const { data } = await http.get(`${BASE_URL}/${id}`)
+  return data?.Data
+}
 
-module.exports  = {
-    getBooks,
-    getBookById,
-    getGenresByIdBook,
+const getGenresByIdBook = async (id) => {
+  const { data } = await http.get(`${BASE_URL}/${id}/genres`)
+  return data?.Data
+}
+
+const searchBooks = async (name) => {
+  const { data } = await http.get(`${BASE_URL}/search/${name}`)
+  return data?.Data || []
+}
+
+module.exports = {
+  getBooks,
+  getBookById,
+  getGenresByIdBook,
+  searchBooks,
 }
